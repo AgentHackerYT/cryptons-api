41ab1a7f49c94bda4b9afecf94651cb2cbe6e181
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2021-08-15 13:51:16 +0300

    Add files via upload

--- src/function/script.js
+++ src/function/script.js
@@ -1,7 +1,8 @@
 const { default: fetch } = require("node-fetch");
 
 async function script(res){
-fetch('https://cryptons.ga/script').then(r => r.json()).then(d =>{
-
+fetch('https://cryptons.ga/api/v1/script').then(r => r.json()).then(d =>{
+res(d.script)
 })
 }
+module.exports = script
